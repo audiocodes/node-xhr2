3b4a66eba3110b708e2f83126c52c9d44be2ad0a
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2024-10-14 12:19:42 +0300

    Types: Expose some private attributes

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
-type XHR = XMLHttpRequest;
+type XHR = XMLHttpRequest & {
+  _url: URL;
+  nodejsHttpAgent?: any;
+  nodejsHttpsAgent?: any;
+  nodejsBaseUrl?: string;
+  _sendHxxpRequest(): void;
+};
 
 export { XHR as XMLHttpRequest };
