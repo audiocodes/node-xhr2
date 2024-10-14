6a7f22037563551b3f95c83de3adff88490dcdf7
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2024-10-14 15:41:30 +0300

    Fix types for real

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
-type XHR = XMLHttpRequest & {
+declare class XHR extends XMLHttpRequest {
   _url: URL;
   nodejsHttpAgent?: any;
   nodejsHttpsAgent?: any;
   nodejsBaseUrl?: string;
   _sendHxxpRequest(): void;
-};
+}
 
 export { XHR as XMLHttpRequest };
